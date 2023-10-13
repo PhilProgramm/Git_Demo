bd02844e547a4bed2a7a2f15d182287a1120bab4
Philippe Willi <philippe.willi@abraxas.ch>
2023-10-13 14:25:06 +0200

    add other endpoint

--- src/main/java/api/Dummyendpoint.java
+++ src/main/java/api/Dummyendpoint.java
@@ -5,4 +5,8 @@ public class Dummyendpoint {
   public void getEndpoint() {
     System.out.println("are you getting this?");
   }
+
+  public void getOtherEndpoint() {
+    System.out.println("are you still getting this?");
+  }
 }
