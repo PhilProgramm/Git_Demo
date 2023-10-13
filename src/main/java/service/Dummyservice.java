76f2850b8fd0701d32bd8926b65d9650fc831c40
Philippe Willi <philippe.willi@abraxas.ch>
2023-10-13 14:26:17 +0200

    add another awesome feature
    (cherry picked from commit c5da91d81bab4f6d488b9c47a086510cec0df2ec)

--- src/main/java/service/Dummyservice.java
+++ src/main/java/service/Dummyservice.java
@@ -2,8 +2,12 @@ package service;
 
 public class Dummyservice {
 
-  public  void myAwesomeFeature() {
+  public void myAwesomeFeature() {
     System.out.println("so awesome!");
+  }
+
+  public void anotherAwesomeFeature() {
+    System.out.println("so great!");
   }
 
 }
